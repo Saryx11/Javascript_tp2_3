08cd98ecb3ef54e6c1c24c524254dde2f1a4caee
Unknown <louis.benjamin@live.fr>
2017-11-06 13:14:24 +0100

    mapping

--- pub-services/index.js
+++ pub-services/index.js
@@ -1,5 +1,6 @@
-var serv = require('./services/pub-services');
-var pubs = serv.listePubs();
-console.log(pubs);
-//var pubsOuverts =serv.listePubsOuvertsJours();
-//console.log(pubsOuverts);
+var pubService = require('./services/pub-services');
+module.exports = {
+services: {
+pubService : pubService
+}
+}
