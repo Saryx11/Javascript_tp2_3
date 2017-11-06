51ab78830b19ddeed3a74811cce234866cbd6267
Unknown <louis.benjamin@live.fr>
2017-11-06 13:13:02 +0100

    functions list and list openned works

--- pub-services/index.js
+++ pub-services/index.js
@@ -1,3 +1,5 @@
 var serv = require('./services/pub-services');
 var pubs = serv.listePubs();
 console.log(pubs);
+//var pubsOuverts =serv.listePubsOuvertsJours();
+//console.log(pubsOuverts);
--- pub-services/services/pub-services.js
+++ pub-services/services/pub-services.js
@@ -1,11 +1,20 @@
 var pubs = require('../mocks/pubs.json');
+var moment = require('moment');
+var pubsOuverts=[];
 function listePubs(){
   return pubs;
 }
-/*function listPubsOuverts(){
-
-}*/
+function listePubsOuvertsJours(){
+  var today = moment().format('dddd');
+  var tomorrow = moment().add(1,'d').format('dddd');
+  for(pub of pubs ){
+    if(pub.openDays.includes(today)){
+      pubsOuverts.push(pub);
+      return pubsOuverts;
+    }
+  }
+}
 module.exports={
   listePubs: listePubs,
-  //listePubsOuverts: listePubsOuverts
+  listePubsOuvertsJours: listePubsOuvertsJours
 }
