a15c2e2a97d493c79c921973fdeedfd31ec82298
Unknown <louis.benjamin@live.fr>
2017-11-06 12:05:49 +0100

    moment library installed

---
+++ pub-services/index.js
@@ -0,0 +1,3 @@
+var serv = require('./services/pub-services');
+var pubs = serv.listePubs();
+console.log(pubs);
---
+++ pub-services/services/pub-services.js
@@ -0,0 +1,11 @@
+var pubs = require('../mocks/pubs.json');
+function listePubs(){
+  return pubs;
+}
+/*function listPubsOuverts(){
+
+}*/
+module.exports={
+  listePubs: listePubs,
+  //listePubsOuverts: listePubsOuverts
+}
